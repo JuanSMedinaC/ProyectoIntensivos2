f178e7e11147021dad0b9c8403f9e4b05cef8583
Santiago0212004 <santiago.barraza1@u.icesi.edu.co>
2023-11-19 20:32:18 -0500

    mongodb working

--- SistemaVentasSID2/src/main/java/com/example/sistemaventassid2/model/mongodb/CustomerMongo.java
+++ SistemaVentasSID2/src/main/java/com/example/sistemaventassid2/model/mongodb/CustomerMongo.java
@@ -1,8 +1,6 @@
 package com.example.sistemaventassid2.model.mongodb;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,24 +8,12 @@ import java.util.List;
 
 @Document(collection = "customers")
 public class CustomerMongo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long customerId;
     private List<String> children;
     private String birthPlace;
     private String location;
     private List<String> hobbies;
     private List<String> sports;
     private String maritalStatus;
-    private List<String> productCategories;
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
 
     public List<String> getChildren() {
         return children;
@@ -75,14 +61,6 @@ public class CustomerMongo {
 
     public void setMaritalStatus(String maritalStatus) {
         this.maritalStatus = maritalStatus;
-    }
-
-    public List<String> getProductCategories() {
-        return productCategories;
-    }
-
-    public void setProductCategories(List<String> productCategories) {
-        this.productCategories = productCategories;
     }
 }
 
